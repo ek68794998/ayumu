3d2e54fd695cceea699dd7edaf1efdad7a33cc0f
Eric <eric.kumlin@gmail.com>
2019-03-08 15:16:45 -0800

    Add grid generation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import React, { Component } from "react";
 import "./App.css";
 
+const COLUMN_COUNT = 7;
+const ROW_COUNT = 4;
+
 class App extends Component {
+    gridData = [];
+
+    constructor(props) {
+        super(props);
+
+        for (let i = 0; i < ROW_COUNT; i++) {
+            let row = [];
+
+            for (let i = 0; i < COLUMN_COUNT; i++) {
+                let cell = Math.round(Math.random() * 100);
+
+                row.push(cell);
+            }
+
+            this.gridData.push(row);
+        }
+    }
+
     render() {
+        console.log(this.gridData);
+
+        const grid = this.gridData.map((row) => {
+            const columns = row.map((cell) => {
+                return (
+                    <td>
+                        <div class="App-cell">{cell}</div>
+                    </td>
+                );
+            });
+
+            return (
+                <tr class="App-row">{columns}</tr>
+            );
+        });
+
         return (
             <div className="App">
-                Foo...
+                <div className="App-container">
+                    <table className="App-grid">
+                        <tbody>{grid}</tbody>
+                    </table>
+                </div>
             </div>
         );
     }
